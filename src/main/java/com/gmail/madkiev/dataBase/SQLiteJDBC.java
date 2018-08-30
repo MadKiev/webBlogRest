e6fbc88fdc729229f5ecdd75bcb0f35399c27e12
Shevchenko3141 <MadKiev@gmail.com>
2018-08-30 14:17:42 +0300

    change JDBC

--- src/main/java/com/gmail/madkiev/dataBase/SQLiteJDBC.java
+++ src/main/java/com/gmail/madkiev/dataBase/SQLiteJDBC.java
@@ -1,178 +1,44 @@
 package com.gmail.madkiev.dataBase;
 
 import java.sql.*;
+public class SQLiteJDBC {
 
-public class SQLiteJDBC
-{
-    public static void main( String args[] )
-    {
-        connectDB();
-        createDB();
-        insertDB();
-        selectDB();
-        updateDB();
-        deleteDB();
-    }
-
-    public static void connectDB()
-    {
-        Connection c = null;
+    /**
+     * Connect to the test.db database
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/blog.db";
+        Connection conn = null;
         try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:myBlog.sqlite");
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println("Opened database successfully");
-    }
-    public static void createDB()
-    {
-        Connection c = null;
-        Statement stmt = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:myBlog.sqlite");
-            System.out.println("Opened database successfully");
-
-            stmt = c.createStatement();
-            String sql = "CREATE TABLE web_blog " +
-                    "(ID INTEGER PRIMARY KEY autoincrement," +
-                    " NAME           CHAR(50)    NOT NULL, " +
-                    " message        TEXT     NOT NULL, ";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-        System.out.println("Table created successfully");
+        return conn;
     }
 
-    public static void insertDB()
-    {
-        Connection c = null;
-        Statement stmt = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:myBlog.sqlite");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
 
-            stmt = c.createStatement();
-            String sql = "INSERT INTO web_blog (NAME,message) " +
-                    "VALUES ('HI', 'My first post!!!', )," +
-                    " ('Pete', 'whats up!!!');";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.commit();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-        System.out.println("Records created successfully");
-    }
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectAll(){
+        String sql = "SELECT id, name, message FROM warehouses";
 
-    public static void selectDB()
-    {
-        Connection c = null;
-        Statement stmt = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:myBlog.sqlite");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
 
-            stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM web_blog;" );
-            while ( rs.next() ) {
-                int id = rs.getInt("id");
-                String  name = rs.getString("name");
-                String  message = rs.getString("message");
-                System.out.println( "ID : " + id );
-                System.out.println( "Name : " + name );
-                System.out.println( "Message : " + message );
-                System.out.println();
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getString("message"));
             }
-            rs.close();
-            stmt.close();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println("Operation done successfully");
     }
 
-    public static void updateDB()
-    {
-        Connection c = null;
-        Statement stmt = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:myBlog.sqlite");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
-
-            stmt = c.createStatement();
-            String sql = "UPDATE web_blog set message = 'This is updated by updateDB()' where ID=1;";
-            stmt.executeUpdate(sql);
-            c.commit();
-
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM web_blog;" );
-            while ( rs.next() ) {
-                int id = rs.getInt("id");
-                String  name = rs.getString("name");
-                String  message = rs.getString("message");
-                System.out.println( "ID : " + id );
-                System.out.println( "Name : " + name );
-                System.out.println( "Message : " + message );
-                System.out.println();
-            }
-            rs.close();
-            stmt.close();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-        System.out.println("Operation done successfully");
-    }
-
-    public static void deleteDB()
-    {
-        Connection c = null;
-        Statement stmt = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:myBlog.sqlite");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
-
-            stmt = c.createStatement();
-            String sql = "DELETE from web_blog where ID=1;";
-            stmt.executeUpdate(sql);
-            c.commit();
-
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM web_blog;" );
-            while ( rs.next() ) {
-                int id = rs.getInt("id");
-                String  name = rs.getString("name");
-                String  message = rs.getString("message");
-                System.out.println( "ID : " + id );
-                System.out.println( "Name : " + name );
-                System.out.println( "Message : " + message );
-                System.out.println();
-            }
-            rs.close();
-            stmt.close();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-        System.out.println("Operation done successfully");
-    }
 }
